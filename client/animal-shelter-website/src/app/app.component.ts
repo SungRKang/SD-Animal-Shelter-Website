a1e247ed60463069b17f31226ddf9f6f2aba7a41
BarrakBM <barrak_mandani@live.com>
2024-02-20 19:35:41 -0700

    initial commit, html file

--- client/animal-shelter-website/src/app/app.component.ts
+++ client/animal-shelter-website/src/app/app.component.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { DogsComponent } from './dogs/dog.component';
 import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule],
+  imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, FooterComponent,RouterOutlet ,CommonModule],
 })
 
 export class AppComponent implements OnInit {
