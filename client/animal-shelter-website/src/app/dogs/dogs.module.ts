03f092bd855995d0eb7ffd2e76e406d750553317
Sungwoo Kang <sungwoo.kang@ucdenver.edu>
2024-02-23 15:40:05 -0700

    navigation and placeholders for pages

--- client/animal-shelter-website/src/app/dogs/dogs.module.ts
+++ client/animal-shelter-website/src/app/dogs/dogs.module.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common';
 import { DogsComponent } from './dog.component';
 
 @NgModule({
-  declarations: [DogsComponent],
   imports: [
     CommonModule
     // other imports if necessary
   ],
-  exports: [DogsComponent] // Export the component so it can be used outside of this module
 })
 export class DogsModule { }
--- client/animal-shelter-website/src/main.ts
+++ client/animal-shelter-website/src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { RouterModule, provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter(routes)
   ]
 }).catch(err => console.error(err));
