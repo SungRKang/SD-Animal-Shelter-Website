c8a3a8d9ba773a7a04ed0969d21836f347accfe3
Sungwoo Kang <sungwoo.kang@ucdenver.edu>
2024-03-02 12:34:26 -0700

    fixed spelling and created the extra components

---
+++ client/animal-shelter-website/src/app/recent-adoptions/recent-adoptions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-adoptions',
+  standalone: true,
+  imports: [],
+  templateUrl: './recent-adoptions.component.html',
+  styleUrl: './recent-adoptions.component.css'
+})
+export class RecentAdoptionsComponent {
+
+}
