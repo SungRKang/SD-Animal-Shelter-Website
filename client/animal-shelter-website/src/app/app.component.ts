fb00adc0fac0351072fe26b856ef1668c269ebed
Natalie Nguyen <Natalie.2.nguyen@ucdenver.edu>
2024-03-02 11:33:07 -0700

    adoptable pets page with filtering buttons (they do not work yet)

--- client/animal-shelter-website/src/app/app.component.ts
+++ client/animal-shelter-website/src/app/app.component.ts
@@ -6,16 +6,17 @@ import { CommonModule } from '@angular/common';
 import { DogsComponent } from './dogs/dog.component';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AdotablePetsComponent } from './adotable-pets/adotable-pets.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { AdoptablePetsComponent } from './adoptable-pets/adoptable-pets.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, NavBarComponent,FooterComponent, AdoptablePetsComponent],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, NavBarComponent, AdotablePetsComponent, FooterComponent, AdoptablePetsComponent]
 })
 
 export class AppComponent implements OnInit {
