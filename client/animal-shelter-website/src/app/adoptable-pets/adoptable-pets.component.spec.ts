738e73f3c5896774cc590743571fb579f4bd9205
Sungwoo Kang <sungwoo.kang@ucdenver.edu>
2024-03-02 12:34:26 -0700

    fixed spelling and created the extra components

--- client/animal-shelter-website/src/app/adoptable-pets/adoptable-pets.component.spec.ts
+++ client/animal-shelter-website/src/app/adoptable-pets/adoptable-pets.component.spec.ts
@@ -21,3 +21,27 @@ describe('AdoptablePetsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdoptablePetsComponent } from './adoptable-pets.component';
+
+describe('AdoptablePetsComponent', () => {
+  let component: AdoptablePetsComponent;
+  let fixture: ComponentFixture<AdoptablePetsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdoptablePetsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AdoptablePetsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
