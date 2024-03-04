e53b5532fa69a7e930a67ceab3ad8f2aa87d2b41
Natalie Nguyen <Natalie.2.nguyen@ucdenver.edu>
2024-03-04 14:40:36 -0700

    FIXED BUTTONS THANKS TO BARRAK

--- client/animal-shelter-website/src/app/app.component.ts
+++ client/animal-shelter-website/src/app/app.component.ts
@@ -6,14 +6,14 @@ import { CommonModule } from '@angular/common';
 import { DogsComponent } from './dogs/dog.component';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { AdotablePetsComponent } from './adotable-pets/adotable-pets.component';
+import { AdoptablePetsComponent } from './adoptable-pets/adoptable-pets.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, NavBarComponent, AdotablePetsComponent],
+  imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, NavBarComponent, AdoptablePetsComponent],
 })
 
 export class AppComponent implements OnInit {
