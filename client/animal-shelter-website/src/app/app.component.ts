a21277b58810956096d04196c6d6664c50a16037
Natalie Nguyen <Natalie.2.nguyen@ucdenver.edu>
2024-03-21 12:25:42 -0600

    Added dog boxes and pop up

--- client/animal-shelter-website/src/app/app.component.ts
+++ client/animal-shelter-website/src/app/app.component.ts
@@ -9,14 +9,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AdoptablePetsComponent } from './adoptable-pets/adoptable-pets.component';
 import { FooterComponent } from './footer/footer.component';
 
-import { AdoptablePetsComponent } from './adoptable-pets/adoptable-pets.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, NavBarComponent, AdotablePetsComponent, FooterComponent, AdoptablePetsComponent]
+    imports: [HttpClientModule, CommonModule, DogsComponent, RouterModule, NavBarComponent, AdoptablePetsComponent, FooterComponent, AdoptablePetsComponent]
 })
 
 export class AppComponent implements OnInit {
--- client/animal-shelter-website/src/app/available-pets/available-pets.component.ts
+++ client/animal-shelter-website/src/app/available-pets/available-pets.component.ts
@@ -1,12 +1,49 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-available-pets',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './available-pets.component.html',
   styleUrl: './available-pets.component.css'
 })
 export class AvailablePetsComponent {
+  sexIsOpen: boolean = false;
+  ageIsOpen: boolean = false; 
+  breedIsOpen: boolean = false; 
+  sizeIsOpen: boolean = false;
+  lengthOfStayIsOpen: boolean = false;
+  sortByIsopen: boolean = false;
+  popUpisOpen: boolean = false; 
+
+  toggleFilter(filterTitle: string) : void{
+    if(filterTitle == "sexIsOpen"){
+      this.sexIsOpen = !this.sexIsOpen;
+    }
+    else if (filterTitle == "ageIsOpen"){
+      this.ageIsOpen = !this.ageIsOpen;
+    }
+    else if (filterTitle == "breedIsOpen"){
+      this.breedIsOpen = !this.breedIsOpen;
+    }
+    else if (filterTitle == "sizeIsOpen"){
+      this.sizeIsOpen = !this.sizeIsOpen;
+    }
+    else if(filterTitle == "lengthOfStayIsOpen"){
+      this.lengthOfStayIsOpen = !this.lengthOfStayIsOpen;
+    }
+    else if (filterTitle == "sortByIsopen"){
+      this.sortByIsopen = !this.sortByIsopen;
+    }
+    
+  }
+
+  openPopUp():void{
+    this.popUpisOpen = !this.popUpisOpen;
+  }
+  closePopUp():void{
+    this.popUpisOpen = false;
+  }
 
 }
