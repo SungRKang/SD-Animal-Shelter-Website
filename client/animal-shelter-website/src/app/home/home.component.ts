b67615337c9b0d8eadd2fa91208aee28c60585d1
adriana-arenivar <122938079+adriana-arenivar@users.noreply.github.com>
2024-03-22 14:54:01 -0600

    new_home

--- client/animal-shelter-website/src/app/home/home.component.ts
+++ client/animal-shelter-website/src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {YouTubePlayerModule} from '@angular/youtube-player';
+import { RouteReuseStrategy, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    YouTubePlayerModule,
+    RouterOutlet, 
+    RouterLink,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
 }
